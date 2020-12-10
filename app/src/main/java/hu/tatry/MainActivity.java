bddc43950c03ee8f8f0602f7b2e6f8279283d3a1
Szilard Orban <orbanszlrd@yahoo.com>
2020-12-10 11:42:47 +0100

    Delete unnecessary files

--- app/src/main/java/hu/tatry/MainActivity.java
+++ app/src/main/java/hu/tatry/MainActivity.java
@@ -2,19 +2,10 @@ package hu.tatry;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import android.view.Window;
 import android.view.WindowManager;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 import android.webkit.WebSettings;
 import android.webkit.WebView;
